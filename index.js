5aaef2db8742a0930acc8d6ff40ae32f3d727f4b
Davronbek2811 <davronjon176@gmail.com>
2025-08-14 07:20:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const user = {}
+
+user.name = prompt('Ismingizni kiriting')
+user.id = prompt('Spasce idingizni kiriting')
+user.password = prompt('Space dagi passwordingizni kiriting')
+
+
+
+console.log(`name: ${user.name} id: ${user.id} passward: ${user.password} `)
